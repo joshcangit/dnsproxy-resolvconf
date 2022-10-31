e1eebec081236c4a200a77b437be78b2e400a5c8
Malik Al <kidznightserver@gmail.com>
2022-11-01 06:10:09 +0700

    use resolv head

--- dnsproxy-helper.sh
+++ dnsproxy-helper.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 cwd=$(dirname $(realpath $0))
 resolv_conf="/etc/adguard/resolv.conf"
-resolv_tail="/etc/resolvconf/resolv.conf.d/tail"
+resolv_head="/etc/resolvconf/resolv.conf.d/head"
 case "${1}" in
 	start)
-	grep -o '^[^#]*' ${resolv_conf} > ${resolv_tail}
-	[ -z $(grep '^options\ edns0$' ${resolv_conf}) ] && echo 'options edns0' >> ${resolv_tail}
+	grep -o '^[^#]*' ${resolv_conf} > ${resolv_head}
+	[ -z $(grep '^options\ edns0$' ${resolv_conf}) ] && echo 'options edns0' >> ${resolv_head}
 	resolvconf -u
 	listen=$(grep -o '^[^#]*' ${resolv_conf} | sed 's/^nameserver\ /--listen=/g')
 	${cwd}/dnsproxy ${listen} --config-path=/etc/adguard/dnsproxy.yml
 	;;
 	stop)
-	> ${resolv_tail}
+	> ${resolv_head}
 	resolvconf -u
 	;;
 esac
